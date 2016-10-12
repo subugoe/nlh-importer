979e5658442af1e2afcf44eb02beeb901e900898
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-10-12 17:44:56 +0200

    Remove to_solr_string

--- src/main/resources/lib/related_item.rb
+++ src/main/resources/lib/related_item.rb
@@ -2,21 +2,4 @@ class RelatedItem
 
   attr_accessor :title, :title_abbreviated, :title_partnumber, :type, :note #, :recordInfo # , :titleInfos
 
-  def to_solr_string
-
-    h = Hash.new
-
-    h.merge!({
-                 :parentdoc_title             => @title,
-                 :parentdoc_title_abbreviated => @title_abbreviated,
-                 :parentdoc_title_partnumber  => @title_partnumber,
-                 :parentdoc_note              => @note,
-                 :parentdoc_type              => @type
-
-             })
-
-    return h
-
-  end
-
 end
