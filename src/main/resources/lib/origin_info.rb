2468c50993600dc3134d18a0956fa4d60290e521
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-10-12 17:42:53 +0200

    Add code to normalize places

--- src/main/resources/lib/origin_info.rb
+++ src/main/resources/lib/origin_info.rb
@@ -2,54 +2,129 @@ class OriginInfo
 
   attr_accessor :place, :edition, :publisher, :date_issued, :date_captured_start, :date_captured_end #,  :issuance, :eventType
 
-  def initialize
-    @publisher  = Array.new
-    @placeTerm  = Array.new
-    @dateIssues = Array.new
-  end
+  # def initialize
+  #   @publisher  = Array.new
+  #   @placeTerm  = Array.new
+  #   @dateIssues = Array.new
+  #
+  # end
+  #
+  # def addPlaceTerm(placeTerm)
+  #   @place << placeTerm
+  # end
+  #
+  # def addPublisher=(publisher)
+  #   @publisher << publisher
+  # end
+  #
+  # def addDateIssued(date)
+  #   @dateIssued << date
+  # end
+  #
+  # def addDateCaptured(date)
+  #   @dateCaptured << date
+  # end
 
-  def addPlaceTerm(placeTerm)
-    @place << placeTerm
-  end
 
-  def addPublisher=(publisher)
-    @publisher << publisher
-  end
+  SWITCHES = {
 
-  def addDateIssued(date)
-    @dateIssued << date
-  end
+      '[Albany]'                            => 'Albany',
 
-  def addDateCaptured(date)
-    @dateCaptured << date
-  end
 
-  def to_solr_string
+      'Boston'                              => 'Boston [N.E.]',
+      '[Boston]'                            => 'Boston [N.E.]',
+      'Boston?'                             => 'Boston [N.E.]',
+      'Printed at Boston'                   => 'Boston [N.E.]',
+      'Boston N.E.'                         => 'Boston [N.E.]',
+      'Boston in N.E'                       => 'Boston [N.E.]',
+      'Boston, in N.E'                      => 'Boston [N.E.]',
+      'Boston in New-England'               => 'Boston [N.E.]',
+      'Boston, New-England'                 => 'Boston [N.E.]',
+      'Boston; New-England'                 => 'Boston [N.E.]',
+      'Bostoniæ [Boston]'                   => 'Boston [N.E.]',
+      'Boston New-England'                  => 'Boston [N.E.]',
 
-    h = Hash.new
+      'Cambridge, Mass'                     => 'Cambridge [Mass.]',
 
-    if (edition == '[Electronic ed.]')
-      # 'placedigitization, yeardigitizationstart, yeardigitizationend, publisherdigitization
-      h.merge!({
-                   :place_digitization     => @place,
-                   :year_digitization_start => @date_captured_start,
-                   :year_digitization_end   => @date_captured_end,
-                   :publisher_digitization => @publisher
-               })
+      'Charleston [S.C.]'                   => 'Charlestown [S.C.]',
+      'Charleston, S.C.'                    => 'Charlestown [S.C.]',
+      'Charles-Town [S.C.]'                 => 'Charlestown [S.C.]',
 
+
+      'Printed at Exeter [N.H.]'            => 'Exeter [N.H.]',
+      'Exeter, N.H'                         => 'Exeter [N.H.]',
+      '[Exeter, N.H.]'                      => 'Exeter [N.H.]',
+
+      '[Hartford]'                          => 'Hartford',
+      'Hartford?'                           => 'Hartford',
+
+      'Lancaster, Pa'                       => 'Lancaster [Pa.]',
+      'Lancäster [Pa.]'                     => 'Lancaster [Pa.]',
+
+      'New Bern, N.C.'                      => 'New Bern [N.C.]',
+
+
+      'Newbury-Port [Mass.]'                => 'Newburyport [Mass.]',
+      '[Newburyport, Mass.]'                => 'Newburyport [Mass.]',
+      '[Newburyport, Conn.]'                => 'Newburyport [Mass.]',
+      'Printed at Newburyport [Mass.]'      => 'Newburyport [Mass.]',
+
+      'New-Haven'                           => 'New Haven',
+      '[New Haven]'                         => 'New Haven',
+      'New Haven?'                          => 'New Haven',
+
+      '[New-London, Conn.]'                 => 'New-London [Conn.]',
+      'N. London [i.e., New London, Conn.]' => 'New-London [Conn.]',
+
+      'Newport, R.I'                        => 'Newport [R.I.]',
+      '[Newport, R.I.]'                     => 'Newport [R.I.]',
+      'Newport, Rhode-Island'               => 'Newport [R.I.]',
+
+
+      'New-York'                            => 'New York',
+      'New-York?'                           => 'New York',
+
+
+      '[Philadelphia]'                      => 'Philadelphia',
+      'Philadelphia?'                       => 'Philadelphia',
+
+      '[Portsmouth, N.H.]'                  => 'Portsmouth [N.H.]',
+      'Portsmouth, N.H.'                    => 'Portsmouth [N.H.]',
+      'Portsmouth, New-Hampshire'           => 'Portsmouth [N.H.]',
+
+
+      '[Providence]'                        => 'Providence',
+      'Providence?'                         => 'Providence',
+      'Printed at Providence'               => 'Providence',
+
+
+      '[Salem, Mass.]'                      => 'Salem [Mass.]',
+      'Salem, Mass'                         => 'Salem [Mass.]',
+
+      'Watertown, Mass'                     => 'Watertown [Mass.]',
+
+      'Williamsburg, VA'                    => 'Williamsburg [VA.]',
+
+      'Wilmington, Del.'                    => 'Wilmington [Del.]',
+      '[Wilmington, Del.]'                  => 'Wilmington [Del.]',
+
+      'Woodbridge, in New-Jersey'           => 'Woodbridge [N.J.]',
+
+      'Printed at Worcester [Mass.]'        => 'Worcester [Mass.]',
+      'Printed at Worcester, Massachusetts' => 'Worcester [Mass.]'
+
+
+  }
+
+
+  def placeFacete
+
+    switch = SWITCHES[@place]
+    if switch == nil
+      return @place
     else
-      # placepublish, yearpublish, publisher
-      h.merge!({
-                   :place_publish => @place,
-                   :year_publish  => @date_issued,
-                   :publisher    => @publisher
-               })
-
+      return switch
     end
-
-
-    return h
-
   end
 
 end
