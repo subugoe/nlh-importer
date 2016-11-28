8c694b2762bbba251c5872167c2f9db8341e4ef2
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-11-28 10:30:05 +0100

    Clean up

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-#source env.sh
-
 docker-compose stop importer
 docker-compose rm -f importer
 mvn clean package
 cp target/nlh-importer-verticle-1.0-SNAPSHOT.jar  docker/lib/
 cp src/main/resources/start.rb docker/
-#docker-compose build --no-cache importer
+#docker-compose build --no-cache --force-rm importer
 docker-compose build --force-rm importer
 docker-compose up -d importer
