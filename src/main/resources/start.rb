6b804dd1af70033916b9c1a75f0bb3a1a0e1d301
GDZ Docker <gdzdevdocker@vm19103.virt.gwdg.de>
2017-01-12 22:32:26 +0100

    Change config (memory usage)

--- src/main/resources/start.rb
+++ src/main/resources/start.rb
@@ -50,7 +50,7 @@ indexer_options = {
 
 
 image_processor_options = {
-    'instances'                  => 20,
+    'instances'                  => 6,
     'worker'                     => true,
 #    'workerPoolName'             => 'image_worker_pool',
 #    'workerPoolSize'             => 1,
@@ -62,7 +62,7 @@ image_processor_options = {
 
 
 mets_copier_options = {
-    'instances'                  => 15,
+    'instances'                  => 7,
     'worker'                     => true,
 #    'workerPoolName'             => 'mets_copier_worker_pool',
 #    'workerPoolSize'             => 1,
@@ -177,23 +177,26 @@ if ENV['PREPARE'] == 'true'
   @rredis.del 'convertpdftoimage'
   @rredis.del 'worksToProcess'
 
+  @rredis.del 'checkmets'
 
 
-  $vertx.deploy_verticle("processors/path_retrieve.rb", retriever_options)
-#  $vertx.deploy_verticle("processors/pdf_path_retriever.rb", pdf_retriever_options)
+#  $vertx.deploy_verticle("processors/path_retrieve.rb", retriever_options)
+  $vertx.deploy_verticle("processors/pdf_path_retriever.rb", pdf_retriever_options)
 #  $vertx.deploy_verticle("processors/tei_path_retriever.rb", tei_retriever_options)
 #  $vertx.deploy_verticle("processors/image_input_paths_mapper.rb", mapper_options)
 #  $vertx.deploy_verticle("processors/retrieve_work_from_outpath.rb", retriever_options)
-#  $vertx.deploy_verticle("processors/check_and_update_all_before_date_indexed.rb", retriever_options)
-#  $vertx.deploy_verticle("processors/check_existence_in_index.rb", retriever_options)
 
 else
 
-    $vertx.deploy_verticle("processors/mets_indexer.rb", indexer_options)
+#    $vertx.deploy_verticle("processors/mets_indexer.rb", indexer_options)
+  #  $vertx.deploy_verticle("processors/check_and_update_all_before_date_indexed.rb", indexer_options)
+  #  $vertx.deploy_verticle("processors/check_existence_in_index.rb", indexer_options)
+
+
   #  $vertx.deploy_verticle("processors/mets_copier.rb", mets_copier_options)
 
   #  $vertx.deploy_verticle("processors/pdf_copier.rb", pdf_copier_options)
-  #  $vertx.deploy_verticle("processors/pdf_converter.rb", pdf_converter_options)
+    $vertx.deploy_verticle("processors/pdf_converter.rb", pdf_converter_options)
 
   #  $vertx.deploy_verticle("processors/tei_copier.rb", tei_copier_options)
 
