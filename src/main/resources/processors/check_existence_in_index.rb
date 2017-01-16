e3df7bf68fa8bef4ae19d06f5d088a048f5a605c
GDZ Docker <gdzdevdocker@vm19103.virt.gwdg.de>
2017-01-16 14:30:12 +0100

    Modify queue name, use solr image format field

--- src/main/resources/processors/check_existence_in_index.rb
+++ src/main/resources/processors/check_existence_in_index.rb
@@ -38,7 +38,7 @@ $vertx.execute_blocking(lambda { |future|
 
   while true do
 
-    res = @rredis.brpop("checkmets")
+    res = @rredis.brpop("metsindexer")
 
     attempts = 0
     begin
@@ -52,19 +52,45 @@ $vertx.execute_blocking(lambda { |future|
 
         # /inpath/METS_Daten/mets_eai2_10C95FDFE3EA5600.xml
         match2  = path.match(/(\S*)\/(\S*)_(\S*)_(\S*)\.(\S*)/)
-        prefix  = match2[2]
+        #prefix  = match2[2]
         product = match2[3]
         work    = match2[4]
-        format  = match2[5]
+        #format  = match2[5]
 
 
         solr_works = @solr.get 'select', :params => {:q => "product:#{product} & work:#{work}", :fl => 'work'}
-        if solr_works['response']['docs'].empty?        
-          pushToQueue([{"path" => path}.to_json], 'metsindexer')
-          pushToQueue([{"path" => path}.to_json], 'metscopier')
+       
+        if solr_works['response']['docs'].empty?
+          @logger.debug "Work #{work} (#{product}) not in index"
+          #pushToQueue([{"path" => path}.to_json], 'reindexmets')
+        else
+
+          solr_page_arr = solr_works['response']['docs'].first['page']
+          image_format = solr_works['response']['docs'].first['image_format']
+
+          images_paths = Dir.glob("#{imageoutpath}/#{product}/#{work}/*", File::FNM_CASEFOLD).select { |e| !File.directory? e }
+          images = images_paths.collect {|p| File.basename(p, '.jpg')}
+
+          pdf_paths = Dir.glob("#{pdfoutpath}/#{product}/#{work}/*", File::FNM_CASEFOLD).select { |e| !File.directory? e }
+          pdfs = pdf_paths.collect { |p| File.basename(p, ".pdf" }
+
+          @logger.debug "Full PDF #{product}/#{work}/#{work}.#{image_format} not found" if !pdfs.include? work
+
+          solr_page_arr.each { |page|
+            if !images.include?(page)
+              @logger.debug "Image #{product}/#{work}/#{page}.#{image_format} not found"
+            end
+
+            if !pdfs.include?(page)
+              @logger.debug "Page PDF #{product}/#{work}/#{page}.pdf not found"
+            end
+
+          }
+
         end
-        
-         @logger.debug "\tFinish checking METS: #{path} \t(#{Java::JavaLang::Thread.current_thread().get_name()})"
+
+        @logger.debug "\tFinish checking METS: #{path} \t(#{Java::JavaLang::Thread.current_thread().get_name()})"
+
 
       else
          @logger.error "Get empty string or nil from redis"
