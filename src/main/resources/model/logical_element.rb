2948e46c35ba9531747ded00a39d0fef2044219b
Joerg Panzer <panzer@sub.uni-goettingen.de>
2017-04-03 14:10:36 +0200

    Modify the label mapping
    add mappings, switch everything to downcase
    set value of type attribute for missing labels

--- src/main/resources/model/logical_element.rb
+++ src/main/resources/model/logical_element.rb
@@ -1,30 +1,130 @@
 class LogicalElement
 
-  attr_accessor :doctype, :id, :label, :dmdid, :admid, :start_page_index, :end_page_index, :part_product, :part_work, :part_key, :level, :parentdoc_work # :volume_uri
+  attr_accessor :doctype, :id, :dmdid, :admid, :start_page_index, :end_page_index, :part_product, :part_work, :part_key, :level, :parentdoc_work # :label, :type, :volume_uri
 
+  STRCTYPE_LABEL_HASH = {
+      "genre"                   => "Genre|Genres",
+      "lang"                    => "Sprache|Sprachen",
+      "abstract"                => "Abstrakt",
+      "acknowledgment"          => "Danksagung",
+      "addendum"                => "Addendum",
+      "additional"              => "Beilage",
+      "advertising"             => "Werbung",
+      "annotation"              => "Annotation",
+      "announcement"            => "Ankündigung",
+      "appendix"                => "Appendix",
+      "article"                 => "Artikel",
+      "attachedwork"            => "Beigefügtes Werk",
+      "beigefuegt"              => "Beigefügt",
+      "bibliography"            => "Bibliographie",
+      "binding"                 => "Einband",
+      "bundle"                  => "Nachlass",
+      "chapter"                 => "Kapitel",
+      "collectivework"          => "Sammelwerk",
+      "comment"                 => "Kommentar",
+      "contained_work"          => "Enthaltenes Werk",
+      "contents"                => "Inhaltsverzeichnis",
+      "corrigenda"              => "Errata",
+      "courtdecision"           => "Rechtsentscheidung",
+      "cover"                   => "Einband",
+      "curriculumvitae"         => "Lebenslauf",
+      "dedication"              => "Widmung",
+      "epilogue"                => "Epilogue",
+      "errata"                  => "Errata",
+      "figure"                  => "Abbildung",
+      "folder"                  => "Mappe",
+      "headword"                => "Stichwort",
+      "illustration"            => "Illustration",
+      "illustrationdescription" => "Bildbeschreibung",
+      "index"                   => "Index",
+      "indexabbreviations"      => "Abkürzungsverzeichnis",
+      "indexauthor"             => "Index of authors",
+      "indexauthors"            => "Autorenverzeichnis",
+      "indexchronological"      => "Chronologisches verzeichnis",
+      "indexfigures"            => "Abbildungsverzeichnis",
+      "indexlocation"           => "Ortsregister",
+      "indexlocations"          => "Ortsregister",
+      "indexnames"              => "Namensregister",
+      "indexofchronology"       => "Index (chronologisch)",
+      "indexoverall"            => "Gesamtindex",
+      "indexpersons"            => "Personenindex",
+      "indexspecial"            => "Spezialverzeichnis",
+      "indexsubject"            => "Fachbegriffsregister",
+      "indextables"             => "Tabellenverzeichnis",
+      "indexvolume"             => "Bandindex",
+      "introduction"            => "Einleitung",
+      "issue"                   => "Heft",
+      "legalcomment"            => "Rechtskommentar",
+      "legalnorm"               => "Rechtsnorm",
+      "letter"                  => "Brief",
+      "lettertoeditor"          => "Brief an den Editor",
+      "list"                    => "Liste",
+      "listofillustrations"     => "Liste der Abbildungen",
+      "listofmaps"              => "Liste der Karten",
+      "listofpublications"      => "Publikationsverzeichnis",
+      "listoftables"            => "Liste der Tabellen",
+      "map"                     => "Karte",
+      "message"                 => "Nachricht",
+      "miscella"                => "Miszelle",
+      "miscelle"                => "Miszelle",
+      "monograph"               => "Monograph",
+      "multivolume_work"        => "Mehrbändiges Werk",
+      "multivolumework"         => "Mehrbändiges Werk",
+      "music"                   => "Musik",
+      "musical_notation"        => "Noten",
+      "notes"                   => "Noten",
+      "obituary"                => "Nachruf",
+      "other"                   => "Sonstiges",
+      "otherdocstrct"           => "Sonstiges",
+      "partofwork"              => "Teilwerk",
+      "periodical"              => "Zeitschrift",
+      "periodicalissue"         => "Zeitschriftenheft",
+      "periodicalpart"          => "Zeitschriftenteil",
+      "periodicalvolume"        => "Zeitschriftenband",
+      "poem"                    => "Gedicht",
+      "preface"                 => "Vorwort",
+      "prepage"                 => "Deckblatt",
+      "remarks"                 => "Anmerkungen",
+      "review"                  => "Rezension",
+      "section"                 => "Abschnitt",
+      "sheetmusic"              => "Musik",
+      "singlemap"               => "Einzelne Karte",
+      "supplement"              => "Beilage",
+      "table"                   => "Tabelle, Liste",
+      "tabledescription"        => "Tabelle der Beschreibungen",
+      "tablelist"               => "Tabelle, Liste",
+      "tableofabbreviations"    => "Abkürzungsverzeichnis",
+      "tableofcontents"         => "Inhaltsverzeichnis",
+      "tableofliteraturerefs"   => "Literaturverzeichnis",
+      "textsection"             => "Textabschnitt",
+      "theses"                  => "Dissertation",
+      "title_page"              => "Titelseite",
+      "titlepage"               => "Titelseite",
+      "unit"                    => "Teil",
+      "volume"                  => "Teil eines mehrbändigen Werkes",
+      "werk_beigefuegt"         => "Beigefügtes Werk",
+      "werk"                    => "Werk"
+  }
 
   STRCTYPE_HASH = {
       "monograph"                 => "monograph",
-      "Monograph"                 => "monograph",
 
       "periodical"                => "periodical",
-      "Periodical"                => "periodical",
 
       "periodicalvolume"          => "volume",
-      "PeriodicalVolume"          => "volume",
-      "PeriodicalIssue"           => "periodical_issue",# todo mapping ?
-      "PeriodicalPart"            => "periodical_part",# todo mapping ?
+      "periodical_volume"          => "volume",
+      "periodicalissue"           => "periodical_issue", # todo mapping ?
+      "periodical_issue"           => "periodical_issue", # todo mapping ?
+      "periodicalpart"            => "periodical_part", # todo mapping ?
+      "periodical_part"            => "periodical_part", # todo mapping ?
 
-      "MultivolumeWork"           => "multivolume_work",
       "multivolumework"           => "multivolume_work",
       "multivolume_work"          => "multivolume_work",
-      "MultiVolumeWork"           => "multivolume_work",
       "volume"                    => "volume",
-      "Volume"                    => "volume",
 
 
-      "contained_work"            => "contained_work",# todo mapping ?
-      "ContainedWork"             => "contained_work",# todo mapping ?
+      "contained_work"            => "contained_work", # todo mapping ?
+      "containedwork"             => "contained_work", # todo mapping ?
 
 
       "bundle"                    => "bundle",
@@ -33,152 +133,179 @@ class LogicalElement
       "manuscript"                => "manuscript",
 
 
-      "Cover"                     => "cover", # todo mapping ?
-      "cover"                     => "cover",# todo mapping ?
-      "Prepage"                   => "prepage",# todo mapping ?
+      "cover"                     => "cover", # todo mapping ?
+      "prepage"                   => "prepage", # todo mapping ?
 
 
       "title_page"                => "title_page",
-      "TitlePage"                 => "title_page",
+      "titlepage"                 => "title_page",
 
 
-      "Chapter"                   => "chapter",# todo mapping ?
-      "chapter"                   => "chapter",# todo mapping ?
+      "chapter"                   => "chapter", # todo mapping ?
 
-      "Section"                   => "section",
       "section"                   => "section",
 
-      "TableOfContents"           => "contents",
+      "tableofcontents"           => "contents",
+      "table_of_contents"           => "contents",
 
       "binding"                   => "binding",
 
-      "DedicationForewordIntro"   => "dedication_foreword_intro",# todo mapping ?
+      "dedicationforewordintro"   => "dedication_foreword_intro", # todo mapping ?
+      "dedication_foreword_intro"   => "dedication_foreword_intro", # todo mapping ?
 
       "dedication"                => "dedication",
-      "Dedication"                => "dedication",
 
-      "TableList"                 => "table_list",# todo mapping ?
-      "Index"                     => "index",
+      "tablelist"                 => "table_list", # todo mapping ?
+      "table_list"                 => "table_list", # todo mapping ?
+
       "index"                     => "index",
-      "IndexSpecial"              => "index_special",# todo mapping ?
-      "IndexLocation"             => "index_location",# todo mapping ?
 
-      "Article"                   => "article",
+      "indexspecial"              => "index_special", # todo mapping ?
+      "index_special"              => "index_special", # todo mapping ?
 
-      "Illustration"              => "illustration",
+      "indexlocation"             => "index_location", # todo mapping ?
+      "index_location"             => "index_location", # todo mapping ?
+
+      "article"                   => "article",
+
       "illustration"              => "illustration",
 
       "contents"                  => "contents",
 
 
-      "Errata"                    => "corrigenda",
+      "errata"                    => "corrigenda",
       "corrigenda"                => "corrigenda",
 
-      "Journal"                   => "periodical", # todo ?
       "journal"                   => "periodical", # todo ?
 
-      "Figure"                    => "figure",# todo mapping ?
+      "figure"                    => "figure", # todo mapping ?
 
 
       "preface"                   => "preface",
-      "Preface"                   => "preface",
 
-      "TextSection"               => "text_section",# todo mapping ?
-
-
-
-      "verse"                     => "verse",# todo mapping ?
-      "Appendix"                  => "appendix",# todo mapping ?
-      "Remarks"                   => "remarks",# todo mapping ?
+      "textsection"               => "text_section", # todo mapping ?
+      "text_section"               => "text_section", # todo mapping ?
 
 
+      "verse"                     => "verse", # todo mapping ?
+      "appendix"                  => "appendix", # todo mapping ?
+      "remarks"                   => "remarks", # todo mapping ?
 
-      "Imprint"                   => "imprint",# todo mapping ?
+
+      "imprint"                   => "imprint", # todo mapping ?
 
 
-
-      "Map"                       => "map",
       "map"                       => "map",
 
-      "Issue"                     => "issue",# todo mapping ?
-      "issue"                     => "issue",# todo mapping ?
+      "issue"                     => "issue", # todo mapping ?
 
-      "Introduction"              => "introduction",# todo mapping ?
+      "introduction"              => "introduction", # todo mapping ?
 
-      "Unit"                      => "unit",# todo mapping ?
+      "unit"                      => "unit", # todo mapping ?
 
-      "Advertising"               => "advertising",# todo mapping ?
-      "AnnouncementAdvertisement" => "announcement_advertisement",# todo mapping ?
-
-
-      "article"                   => "article",# todo mapping ?
+      "advertising"               => "advertising", # todo mapping ?
+      "announcementadvertisement" => "announcement_advertisement", # todo mapping ?
+      "announcement_advertisement" => "announcement_advertisement", # todo mapping ?
 
 
-      "Table"                     => "table",
       "table"                     => "table",
 
-      "IllustrationDescription"   => "illustration_description",# todo mapping ?
-      "Review"                    => "review",# todo mapping ?
+      "illustrationdescription"   => "illustration_description", # todo mapping ?
+      "illustration_description"   => "illustration_description", # todo mapping ?
 
+      "review"                    => "review", # todo mapping ?
+
+      "engravedtitlepage"        => "engraved_titlepage",
       "engraved_titlepage"        => "engraved_titlepage",
 
-      "Message"                   => "message",# todo mapping ?
+      "message"                   => "message", # todo mapping ?
 
-      "additional"                => "additional",# todo mapping ?
+      "additional"                => "additional", # todo mapping ?
 
-      "IndexPersons"              => "index_persons",# todo mapping ?
-      "IndexSubject"              => "index_subject",# todo mapping ?
-      "Entry"                     => "entry",# todo mapping ?
-      "Addendum"                  => "addendum",# todo mapping ?
-      "TableOfLiteratureRefs"     => "table_of_literature_refs",# todo mapping ?
-      "IndexOverall"              => "index_overall",# todo mapping ?
-      "AttachedWork"              => "attached_work",# todo mapping ?
+      "indexpersons"              => "index_persons", # todo mapping ?
+      "index_persons"              => "index_persons", # todo mapping ?
 
-      "Announcement"              => "announcement",# todo mapping ?
-      "IndexAuthor"               => "index_author",# todo mapping ?
-      "Letter"                    => "letter",# todo mapping ?
-      "Epilogue"                  => "epilogue",# todo mapping ?
-      "List"                      => "list",# todo mapping ?
-      "Title"                     => "title",# todo mapping ?
-      "PartOfWork"                => "part_of_work",# todo mapping ?
+      "indexsubject"              => "index_subject", # todo mapping ?
+      "index_subject"              => "index_subject", # todo mapping ?
 
-      "ImprintColophon"           => "imprint_colophon",# todo mapping ?
+      "entry"                     => "entry", # todo mapping ?
+      "addendum"                  => "addendum", # todo mapping ?
+
+      "tableofliteraturerefs"     => "table_of_literature_refs", # todo mapping ?
+      "table_of_literature_refs"     => "table_of_literature_refs", # todo mapping ?
+
+      "indexoverall"              => "index_overall", # todo mapping ?
+      "index_overall"              => "index_overall", # todo mapping ?
+
+      "attachedwork"              => "attached_work", # todo mapping ?
+      "attached_work"              => "attached_work", # todo mapping ?
+
+      "announcement"              => "announcement", # todo mapping ?
+
+      "indexauthor"               => "index_author", # todo mapping ?
+      "index_author"               => "index_author", # todo mapping ?
+
+      "letter"                    => "letter", # todo mapping ?
+      "epilogue"                  => "epilogue", # todo mapping ?
+      "list"                      => "list", # todo mapping ?
+      "title"                     => "title", # todo mapping ?
+
+      "partofwork"                => "part_of_work", # todo mapping ?
+      "part_of_work"                => "part_of_work", # todo mapping ?
+
+      "imprintcolophon"           => "imprint_colophon", # todo mapping ?
+      "imprint_colophon"           => "imprint_colophon", # todo mapping ?
       "colophon"                  => "colophon",
 
-      "Obituary"                  => "obituary",# todo mapping ?
+      "obituary"                  => "obituary", # todo mapping ?
+
+      "other"                     => "other", # todo mapping ?
+
+      "otherdocstrct"             => "other_docstrct", # todo mapping ?
+      "other_doc_strct"             => "other_docstrct", # todo mapping ?
+
+      "otherdocstruct"            => "other_docstrct", # todo mapping ?
+      "other_doc_struct"            => "other_docstrct", # todo mapping ?
+
+      "miscellany"                => "miscellany", # todo mapping ?
+      "miscella"                  => "miscellany", # todo mapping ?
 
 
-      "other"                     => "other",# todo mapping ?
-      "Other"                     => "other",# todo mapping ?
-      "OtherDocStrct"             => "other_docstrct",# todo mapping ?
-      "OtherDocStruct"            => "other_docstrct",# todo mapping ?
+      "music"                     => "music", # todo mapping ?
 
-      "Miscellany"                => "miscellany",# todo mapping ?
-      "Miscella"                  => "miscellany",# todo mapping ?
+      "sheetmusic"                => "sheet_music", # todo mapping ?
+      "sheet_music"                => "sheet_music", # todo mapping ?
 
-
-      "Music"                     => "music",# todo mapping ?
-      "SheetMusic"                => "sheet_music",# todo mapping ?
+      "musicalnotation"          => "musical_notation",
       "musical_notation"          => "musical_notation",
 
-      "Supplement"                => "supplement",# todo mapping ?
-      "Acknowledgment"            => "acknowledgment",# todo mapping ?
-      "Bibliography"              => "bibliography",# todo mapping ?
+      "supplement"                => "supplement", # todo mapping ?
 
-      "Abstract"                  => "abstract",# todo mapping ?
-      "CurriculumVitae"           => "curriculum_vitae",# todo mapping ?
-      "TableOfAbbreviations"      => "table_of_abbreviations",# todo mapping ?
+      "acknowledgment"            => "acknowledgment", # todo mapping ?
+      "bibliography"              => "bibliography", # todo mapping ?
+
+      "abstract"                  => "abstract", # todo mapping ?
+
+      "curriculumvitae"           => "curriculum_vitae", # todo mapping ?
+      "curriculum_vitae"           => "curriculum_vitae", # todo mapping ?
+
+      "tableofabbreviations"      => "table_of_abbreviations", # todo mapping ?
+      "table_of_abbreviations"      => "table_of_abbreviations", # todo mapping ?
 
 
-      "Headword"                  => "headword",# todo mapping ?
-      "Theses"                    => "theses",# todo mapping ?
-      "ListOfPublications"        => "list_of_publications",# todo mapping ?
-      "TableDescription"          => "table_description"# todo mapping ?
+      "headword"                  => "headword", # todo mapping ?
+      "theses"                    => "theses", # todo mapping ?
+
+      "listofpublications"        => "list_of_publications", # todo mapping ?
+      "list_of_publications"        => "list_of_publications", # todo mapping ?
+
+      "tabledescription"          => "table_description", # todo mapping ?
+      "table_description"          => "table_description" # todo mapping ?
 
   }
 
   def type=(type)
-    t = STRCTYPE_HASH[type]
+    t = STRCTYPE_HASH[type.downcase]
     t = type if t == nil
 
     @type = t
@@ -186,6 +313,18 @@ class LogicalElement
 
   def type
     @type
+  end
+
+
+  def label=(label)
+    l = STRCTYPE_LABEL_HASH[label.downcase]
+    l = label if l == nil
+
+    @label = l
+  end
+
+  def label
+    @label
   end
 
 
