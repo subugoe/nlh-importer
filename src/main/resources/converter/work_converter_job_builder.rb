f2900cc9d9dd55d992db4ea64080c69891355232
Jörg-H Panzer <jpanzer@Jorg.sub.uni-goettingen.de>
2017-10-16 14:01:22 +0200

    Apply stashed code

--- src/main/resources/converter/work_converter_job_builder.rb
+++ src/main/resources/converter/work_converter_job_builder.rb
@@ -27,12 +27,56 @@ require 'converter/work_converter'
 @logger.debug "[converter_job_builder] Running in #{Java::JavaLang::Thread.current_thread().get_name()}"
 
 @queue  = ENV['REDIS_WORK_CONVERT_QUEUE']
+@unique_queue  = ENV['REDIS_UNIQUE_QUEUE']
+
 @rredis = Redis.new(
     :host => ENV['REDIS_HOST'],
     :port => ENV['REDIS_EXTERNAL_PORT'].to_i,
     :db   => ENV['REDIS_DB'].to_i
 )
 
+
+=begin
+$vertx.event_bus().consumer("image.load") {|message|
+
+  @logger.debug "[img_to_pdf_converter_job_builder] GC.start #{GC.start} -> max: #{GC.stat[:max]}"
+
+  begin
+    body = message.body()
+
+    if (body != '' && body != nil)
+
+      json = JSON.parse body
+
+      Benchmark.bm(7) do |x|
+        x.report("Begin benchmark") {
+          converter = WorkConverter.new
+          converter.process_response(res)
+        }
+      end
+    else
+      raise "Could not process empty string or nil"
+    end
+  rescue Exception => e
+    @logger.error "[img_to_pdf_converter_job_builder] Processing problem with '#{json}' \t#{e.message}\n\t#{e.backtrace}"
+    @file_logger.error "[img_to_pdf_converter_job_builder] Processing problem with '#{json}' \t#{e.message}\n\t#{e.backtrace}"
+  end
+}
+=end
+
+
+def pushToQueue(queue, field, value)
+  @rredis.hset(queue, field, value)
+end
+
+
+
+def removeQueue(queue)
+  keys = @rredis.hkeys(queue)
+  unless keys.empty?
+    @rredis.hdel(queue, keys)
+  end
+end
 
 $vertx.execute_blocking(lambda {|future|
 
@@ -42,7 +86,19 @@ $vertx.execute_blocking(lambda {|future|
 
     while true do
 
+      # {"id":"PPN826737668___LOG_0000","context":"gdz"}
       res = @rredis.brpop(@queue) # , :timeout => nil)
+
+      msg  = res[1]
+      json = JSON.parse msg
+      id      = json['id']
+
+      exist = @rredis.hget(@unique_queue, id)
+      unless exist == nil
+        @logger.debug "[converter_job_builder] Job for #{id} already started, process next"
+        next
+      end
+      @rredis.hset(@unique_queue, id, -1)
 
       converter = WorkConverter.new
       converter.process_response(res)
