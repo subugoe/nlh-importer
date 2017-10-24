5377af0cea40e25c828066807e0dca42391a454b
Jörg-H Panzer <jpanzer@jorg.speedport.ip>
2017-10-24 21:19:07 +0200

    Add status info to response
    add HTTP status code
    add processing status
      -1 -> error
       0 -> staged
       1-100 percentage progress
    add message

--- src/main/resources/services/converter_service_verticle.rb
+++ src/main/resources/services/converter_service_verticle.rb
@@ -28,8 +28,8 @@ def pushToQueue(queue, arr)
   @rredis.lpush(queue, arr)
 end
 
-def send_error(status_code, response)
-  response.set_status_code(status_code).end
+def send_status(status_code, response, msg_hsh)
+  response.set_status_code(status_code).end(msg_hsh.to_json)
 end
 
 
@@ -37,6 +37,7 @@ router = VertxWeb::Router.router($vertx)
 router.route().handler(&VertxWeb::BodyHandler.create().method(:handle))
 
 # POST 134.76.18.25:8083     /api/converter/jobs
+# Request
 # {
 #     "document": "PPN591416441",
 #     "log": "PPN591416441",
@@ -50,30 +51,94 @@ router.route().handler(&VertxWeb::BodyHandler.create().method(:handle))
 #     "log": "LOG_0007",
 #     "context": "gdz"
 # }
-router.post("/api/converter/jobs").blocking_handler(lambda { |routingContext|
+#
+# Response
+# {
+#     "status":"<percentage> | -1>"
+# }
+router.post("/api/converter/jobs").blocking_handler(lambda {|routingContext|
 
   begin
-    hsh = routingContext.get_body_as_json
 
-    # hsh:          {"id"=>"PPN826737668___LOG_0000", "context"=>"gdz"}
-    # hsh.to_json:  {"id":"PPN826737668___LOG_0000","context":"gdz"}
+    hsh      = routingContext.get_body_as_json
+    response = routingContext.response
 
     if hsh == nil
       @logger.error("[converter_service] Expected JSON body missing")
       @file_logger.error("[converter_service]  Expected JSON body missing")
-      send_error(400, response)
+
+      # TODO check response codes
+
+      send_status(400, response, {"status" => -1, "msg" => "Requst body missing"})
+
     else
       @logger.info("[converter_service] Got message: \t#{hsh}")
 
-      pushToQueue(@queue, [hsh.to_json])
+      id      = hsh['document']
+      log     = hsh['log']
+      context = hsh['context']
+
+      send_status(400, response, {"status" => -1, "msg" => "Required parameters missed (document, log, context)"}) if (id == nil) || (log == nil) || (context == nil)
+
+      log_id = "#{id}___#{log}"
+
+      exist = @rredis.hget(@unique_queue, log_id)
+
+      if exist != nil
+        @logger.debug "[converter_job_builder] Job for #{log_id} already started, process next"
+
+        # conversion error
+        if @rredis.hget(log_id, 'err') != nil
+          @logger.error("[converter_service] Errors in queue #{log_id}, job not staged")
+          @file_logger.error("[converter_service] Errors in queue #{log_id}, job not staged")
+
+          send_status(400, response, {"status" => -1, "msg" => "Conversion errors"})
+
+        else
+
+          resp = (@solr.get 'select', :params => {:q => "id:#{id}", :fl => "page   log_id   log_start_page_index   log_end_page_index doctype"})['response']['docs'].first
+
+          # no conversion of doctype != 'work'
+          send_status(400, response, {"status" => -4}) if resp['doctype'] == 'work'
+
+          if request_logical_part
+
+            log_id_index = resp['log_id'].index log
+
+            log_start_page_index = (resp['log_start_page_index'][log_id_index])-1
+            log_end_page_index   = (resp['log_end_page_index'][log_id_index])-1
+
+            size = log_end_page_index.to_i - log_start_page_index.to_i
+
+          else
+            size = resp['page'].size
+          end
+
+          converted = (@rredis.hkeys(log_id)).size-1 # -1, since the field "0" is not related to a real page, it sets a lock on the id
+
+          i = converted * 100 / size
+
+          send_status(200, response, {"status" => i})
+
+        end
+
+      else
+        @rredis.hset(@unique_queue, log_id, 0)
+        pushToQueue(@queue, [hsh.to_json])
+        send_status(200, response, {"status" => 0, "msg" => "Work #{log_id} staged for conversion"})
+      end
+
     end
 
   rescue Exception => e
     @logger.error("[converter_service] Problem with request body \t#{e.message}")
     @file_logger.error("[converter_service] Problem with request body \t#{e.message}\n\t#{e.backtrace}")
+
+    # any error
+    send_status(400, response, {"status" => -1, "msg" => "Request could not processed"})
   end
 
-  routingContext.response.end
+  #routingContext.response.end
 
 }, false)
 
