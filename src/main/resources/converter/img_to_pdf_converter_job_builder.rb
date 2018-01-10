655623565ebcda479771153f6eed39116873f239
Jörg-H Panzer <jpanzer@Jorg.local>
2018-01-10 08:53:06 +0100

    Code formatting and cleanup

--- src/main/resources/converter/img_to_pdf_converter_job_builder.rb
+++ src/main/resources/converter/img_to_pdf_converter_job_builder.rb
@@ -21,7 +21,6 @@ require 'converter/img_to_pdf_converter'
     :db   => ENV['REDIS_DB'].to_i
 )
 
-
 $vertx.execute_blocking(lambda {|future|
 
   GC.start
@@ -30,15 +29,11 @@ $vertx.execute_blocking(lambda {|future|
   begin
 
     while true do
-
       res = @rredis.brpop(@queue) #, :timeout => nil)
-
       msg  = res[1]
       json = JSON.parse msg
-
       converter = ImgToPdfConverter.new
       converter.process_response(json)
-
     end
 
   rescue Exception => e
@@ -53,31 +48,3 @@ $vertx.execute_blocking(lambda {|future|
 }) {|res_err, res|
   #
 }
-
-=begin
-$vertx.event_bus().consumer("image.load") {|message|
-
-  @logger.debug "[img_converter_job_builder] GC.start #{GC.start} -> max: #{GC.stat[:max]}"
-
-  begin
-    body = message.body()
-
-    if (body != '' && body != nil)
-
-      json = JSON.parse body
-
-      Benchmark.bm(7) do |x|
-        x.report("Begin benchmark") {
-          converter = ImgToPdfConverter.new
-          converter.process_response(json)
-        }
-      end
-    else
-      raise "Could not process empty string or nil"
-    end
-  rescue Exception => e
-    @logger.error "[img_converter_job_builder] Processing problem with '#{json}' \t#{e.message}\n\t#{e.backtrace}"
-    @file_logger.error "[img_converter_job_builder] Processing problem with '#{json}' \t#{e.message}\n\t#{e.backtrace}"
-  end
-}
-=end
