ead05ab3374a69efc883992be97718f49b283e9e
Joerg-Holger Panzer <panzer@sub.uni-goettingen.de>
2020-02-21 16:45:58 +0100

    Change/Add Gems

--- src/main/resources/start_img_full_converter.rb
+++ src/main/resources/start_img_full_converter.rb
@@ -3,25 +3,14 @@ require 'vertx/vertx'
 require 'rubygems'
 require 'logger'
 require 'gelf'
-
+require "converter/img_to_pdf_converter_job_builder"
 
 @logger       = Logger.new(STDOUT)
 @logger.level = ENV['DEBUG_MODE'].to_i
 @logger.info "[start_converter] Running in #{Java::JavaLang::Thread.current_thread().get_name()}"
 
 
-job_builder_options = {
-    'instances'                  => 1,
-    'worker'                     => true,
-    'blockedThreadCheckInterval' => 3600000,
-    'warningExceptionTime'       => 3600000,
-    'maxWorkerExecuteTime'       => 3400000000000,
-    'maxEventLoopExecuteTime'    => 600000000000,
-    'GEM_PATH'                   => '/opt/jruby/lib/ruby/gems/shared/gems'
-}
-
-
-converter_options = {
+img_job_builder_options = {
     'instances'                  => 4,
     'worker'                     => true,
     'blockedThreadCheckInterval' => 3600000,
@@ -32,9 +21,6 @@ converter_options = {
 }
 
 
-# converter service endpoint
-$vertx.deploy_verticle("converter/work_converter_job_builder.rb", job_builder_options)
-
 # converter loader eb
-$vertx.deploy_verticle("converter/img_to_pdf_converter_job_builder.rb", converter_options)
+$vertx.deploy_verticle("converter/img_to_pdf_converter_job_builder.rb", img_job_builder_options)
 
---
+++ src/main/resources/start_work_converter.rb
@@ -0,0 +1,27 @@
+require 'vertx/vertx'
+
+require 'rubygems'
+require 'logger'
+require 'gelf'
+require "converter/work_converter_job_builder"
+
+@logger       = Logger.new(STDOUT)
+@logger.level = ENV['DEBUG_MODE'].to_i
+@logger.info "[start_converter] Running in #{Java::JavaLang::Thread.current_thread().get_name()}"
+
+
+work_job_builder_options = {
+    'instances'                  => 1,
+    'worker'                     => true,
+    'blockedThreadCheckInterval' => 3600000,
+    'warningExceptionTime'       => 3600000,
+    'maxWorkerExecuteTime'       => 3400000000000,
+    'maxEventLoopExecuteTime'    => 600000000000,
+    'GEM_PATH'                   => '/opt/jruby/lib/ruby/gems/shared/gems'
+}
+
+
+# converter service endpoint
+$vertx.deploy_verticle("converter/work_converter_job_builder.rb", work_job_builder_options)
+
+
