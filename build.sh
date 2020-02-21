6a2149ee82e3424c53cf3242cc8498e2c271a360
Joerg-Holger Panzer <panzer@sub.uni-goettingen.de>
2020-02-21 16:56:46 +0100

    Modify project configuration

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 source .cfg
 
-if [ -z "$solr_core" ]
+if [ -z "$solr_gdz_core" ]
 then
-    export solr_core="gdz"
+    export solr_gdz_core="gdz"
 fi
 
 
-if [ -z "$solr_core2" ]
+if [ -z "$solr_gdz_core2" ]
 then
-    export solr_core2="gdz_tmp"
+    export solr_gdz_core2="gdz_tmp"
 fi
 
+export solr_nlh_core="nlh"
 
 if [ -z "$environment" ]
 then
@@ -21,31 +22,31 @@ fi
 
 export myUID=`id -u ${whoami}`
 
-export SOLR_PORT=8983
-export SOLR_EXTERNAL_PORT=8443
+export SOLR_GDZ_PORT=8983
+export SOLR_GDZ_EXTERNAL_PORT=8900
 
 export SOLR_JAVA_MEM='-Xms512M -Xmx7G'
 
 export SERVICE_VERTICLE=start_services.rb
 export INDEXER_VERTICLE=start_indexer.rb
-export CONVERTER_VERTICLE=start_converter.rb
+export CONVERTER_WORK_VERTICLE=start_work_converter.rb
+export CONVERTER_IMG_LOG_VERTICLE=start_img_log_converter.rb
+export CONVERTER_IMG_FULL_VERTICLE=start_img_full_converter.rb
 
 export VERTICLE_HOME=/usr/verticles
 export CONVERTER_VERTX_OPTIONS="--workerPoolSize 40 --blockedThreadCheckInterval 3600000 --maxEventLoopExecuteTime 600000000000 --maxWorkerExecuteTime 3400000000000 maxEventLoopExecuteTime 600000000000"
 
-export LOGO_PATH=${VERTICLE_HOME}/image/sub-blue.svg
+export GDZ_LOGO_PATH=${VERTICLE_HOME}/image/sub-blue.svg
+export NLH_LOGO_PATH=${VERTICLE_HOME}/image/nlh_logo_2.png
+export NLH_FOOTER_PATH=${VERTICLE_HOME}/image/nlh_products_footer.png
 export FONT_PATH=${VERTICLE_HOME}/font
 
 export PATH="$HOME/.cargo/bin:/usr/local/go/bin:$PATH"
 export GOPATH=/Users/jpanzer/Documents/projects/test/go
 export GOROOT=/usr/local/go
 
-mkdir -p data/solr/$solr_core/data/
-mkdir -p data/solr/$solr_core2/data/
-
-cp src/main/resources/start_services.rb docker/
-cp src/main/resources/start_indexer.rb docker/
-cp src/main/resources/start_converter.rb docker/
+mkdir -p data/solr/$solr_gdz_core/data/
+mkdir -p data/solr/$solr_gdz_core2/data/
 
 cp docker-compose_orig.yml docker-compose.yml
 
@@ -63,41 +64,58 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     sed -i '' "s|<verticle_home>|${VERTICLE_HOME}|"         ./docker/Dockerfile
 
-    sed -i '' "s|<solr_core>|${solr_core}|g"                ./docker-compose.yml
-    sed -i '' "s|<solr_core2>|${solr_core2}|g"              ./docker-compose.yml
+    sed -i '' "s|<solr_gdz_core>|${solr_gdz_core}|g"                ./docker-compose.yml
+    sed -i '' "s|<solr_gdz_core2>|${solr_gdz_core2}|g"              ./docker-compose.yml
 
-    sed -i '' "s|<solr_core>|${solr_core}|"                 ./docker/solr/Dockerfile
-    sed -i '' "s|<solr_core2>|${solr_core2}|"               ./docker/solr/Dockerfile
-    sed -i '' "s|<solr_port>|${SOLR_PORT}|"                 ./docker/solr/Dockerfile
+    sed -i '' "s|<solr_gdz_core>|${solr_gdz_core}|"                 ./docker/solr/Dockerfile
+    sed -i '' "s|<solr_gdz_core2>|${solr_gdz_core2}|"               ./docker/solr/Dockerfile
+    sed -i '' "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|"                 ./docker/solr/Dockerfile
 
-    sed -i '' "s|<solr_core>|${solr_core}|"                 ./docker/solr/config/sub/core.properties
-    sed -i '' "s|<solr_core>|${solr_core2}|"                ./docker/solr/config/sub/core2.properties
+    sed -i '' "s|<solr_gdz_core>|${solr_gdz_core}|"                 ./docker/solr/config/sub/core.properties
+    sed -i '' "s|<solr_gdz_core>|${solr_gdz_core2}|"                ./docker/solr/config/sub/core2.properties
 
-    sed -i '' "s|<solr_port>|${SOLR_PORT}|g"                ./docker/solr/config/sub/jetty.xml
+    sed -i '' "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|g"                ./docker/solr/config/sub/jetty.xml
 
     sed -i '' "s|<solr_java_mem>|${SOLR_JAVA_MEM}|"         ./docker/solr/config/solr.in.sh
-    sed -i '' "s|<solr_port>|${SOLR_PORT}|"                 ./docker/solr/config/solr.in.sh
+    sed -i '' "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|"                 ./docker/solr/config/solr.in.sh
 
-    sed -i '' "s|<solr_port>|${SOLR_PORT}|g"                                .env
-    sed -i '' "s|<solr_external_port>|${SOLR_EXTERNAL_PORT}|g"              .env
-    sed -i '' "s|<logo_path>|${LOGO_PATH}|g"                                .env
-    sed -i '' "s|<font_path>|${FONT_PATH}|g"                                .env
-    sed -i '' "s|<s3_provider>|${S3_PROVIDER}|g"                            .env
-    sed -i '' "s|<s3_aws_access_key_id>|${S3_AWS_ACCESS_KEY_ID}|g"          .env
-    sed -i '' "s|<s3_aws_secret_access_key>|${S3_AWS_SECRET_ACCESS_KEY}|g"  .env
-    sed -i '' "s|<s3_endpoint>|${S3_ENDPOINT}|g"                            .env
+    sed -i '' "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|g"                                .env
+    sed -i '' "s|<solr_gdz_external_port>|${SOLR_GDZ_EXTERNAL_PORT}|g"              .env
+    sed -i '' "s|<gdz_logo_path>|${GDZ_LOGO_PATH}|g"                                        .env
+    sed -i '' "s|<nlh_logo_path>|${NLH_LOGO_PATH}|g"                                        .env
+    sed -i '' "s|<nlh_footer_path>|${NLH_FOOTER_PATH}|g"                                        .env
+    sed -i '' "s|<font_path>|${FONT_PATH}|g"                                        .env
+    sed -i '' "s|<s3_sub_provider>|${S3_SUB_PROVIDER}|g"                            .env
+    sed -i '' "s|<s3_sub_aws_access_key_id>|${S3_SUB_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i '' "s|<s3_sub_aws_secret_access_key>|${S3_SUB_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i '' "s|<s3_sub_region>|${S3_SUB_REGION}|g"                                .env
+    sed -i '' "s|<s3_sub_endpoint>|${S3_SUB_ENDPOINT}|g"                            .env
+    sed -i '' "s|<s3_nlh_provider>|${S3_NLH_PROVIDER}|g"                            .env
+    sed -i '' "s|<s3_nlh_aws_access_key_id>|${S3_NLH_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i '' "s|<s3_nlh_aws_secret_access_key>|${S3_NLH_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i '' "s|<s3_nlh_region>|${S3_NLH_REGION}|g"                                .env
+    sed -i '' "s|<s3_nlh_endpoint>|${S3_NLH_ENDPOINT}|g"                            .env
+    sed -i '' "s|<s3_digizeit_provider>|${S3_DIGIZEIT_PROVIDER}|g"                            .env
+    sed -i '' "s|<s3_digizeit_aws_access_key_id>|${S3_DIGIZEIT_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i '' "s|<s3_digizeit_aws_secret_access_key>|${S3_DIGIZEIT_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i '' "s|<s3_digizeit_region>|${S3_DIGIZEIT_REGION}|g"                            .env
+    sed -i '' "s|<s3_digizeit_endpoint>|${S3_DIGIZEIT_ENDPOINT}|g"                            .env
     sed -i '' "s|<solr_java_mem>|${SOLR_JAVA_MEM}|"                         .env
     sed -i '' "s|<local_host>|${LOCAL_HOST}|"                               .env
     sed -i '' "s|<service_host>|${SERVICE_HOST}|"                           .env
     sed -i '' "s|<redis_host>|${REDIS_HOST}|"                               .env
-    sed -i '' "s|<solr_host>|${SOLR_HOST}|"                                 .env
+    sed -i '' "s|<solr_gdz_host>|${SOLR_GDZ_HOST}|"                                 .env
+    sed -i '' "s|<solr_nlh_host>|${SOLR_NLH_HOST}|"                                 .env
     sed -i '' "s|<graylog_host>|${GRAYLOG_HOST}|"                           .env
     sed -i '' "s|<graylog_port>|${GRAYLOG_PORT}|"                           .env
-    sed -i '' "s|<solr_core>|${solr_core}|"                                 .env
-    sed -i '' "s|<solr_core2>|${solr_core2}|"                               .env
+    sed -i '' "s|<solr_gdz_core>|${solr_gdz_core}|"                                 .env
+    sed -i '' "s|<solr_gdz_core2>|${solr_gdz_core2}|"                               .env
+    sed -i '' "s|<solr_nlh_core>|${solr_nlh_core}|"                                 .env
     sed -i '' "s|<service_verticle>|${SERVICE_VERTICLE}|"                   .env
     sed -i '' "s|<indexer_verticle>|${INDEXER_VERTICLE}|"                   .env
-    sed -i '' "s|<converter_verticle>|${CONVERTER_VERTICLE}|"               .env
+    sed -i '' "s|<converter_work_verticle>|${CONVERTER_WORK_VERTICLE}|"               .env
+    sed -i '' "s|<converter_img_log_verticle>|${CONVERTER_IMG_LOG_VERTICLE}|"               .env
+    sed -i '' "s|<converter_img_full_verticle>|${CONVERTER_IMG_FULL_VERTICLE}|"               .env
     sed -i '' "s|<verticle_home>|${VERTICLE_HOME}|"                         .env
     sed -i '' "s|<converter_vertx_options>|${CONVERTER_VERTX_OPTIONS}|"     .env
 
@@ -105,41 +123,58 @@ else
     sed -i "s|<uid>|${myUID}|"                               ./docker/Dockerfile
     sed -i "s|<verticle_home>|${VERTICLE_HOME}|"             ./docker/Dockerfile
 
-    sed -i "s|<solr_core>|${solr_core}|g"                    ./docker-compose.yml
-    sed -i "s|<solr_core2>|${solr_core2}|g"                  ./docker-compose.yml
+    sed -i "s|<solr_gdz_core>|${solr_gdz_core}|g"                    ./docker-compose.yml
+    sed -i "s|<solr_gdz_core2>|${solr_gdz_core2}|g"                  ./docker-compose.yml
 
-    sed -i "s|<solr_core>|${solr_core}|"                     ./docker/solr/Dockerfile
-    sed -i "s|<solr_core2>|${solr_core2}|"                   ./docker/solr/Dockerfile
-    sed -i "s|<solr_port>|${SOLR_PORT}|"                     ./docker/solr/Dockerfile
+    sed -i "s|<solr_gdz_core>|${solr_gdz_core}|"                     ./docker/solr/Dockerfile
+    sed -i "s|<solr_gdz_core2>|${solr_gdz_core2}|"                   ./docker/solr/Dockerfile
+    sed -i "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|"                     ./docker/solr/Dockerfile
 
-    sed -i "s|<solr_core>|${solr_core}|"                     ./docker/solr/config/sub/core.properties
-    sed -i "s|<solr_core>|${solr_core2}|"                    ./docker/solr/config/sub/core2.properties
+    sed -i "s|<solr_gdz_core>|${solr_gdz_core}|"                     ./docker/solr/config/sub/core.properties
+    sed -i "s|<solr_gdz_core>|${solr_gdz_core2}|"                    ./docker/solr/config/sub/core2.properties
 
-    sed -i "s|<solr_port>|${SOLR_PORT}|g"                    ./docker/solr/config/sub/jetty.xml
+    sed -i "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|g"                    ./docker/solr/config/sub/jetty.xml
 
     sed -i "s|<solr_java_mem>|${SOLR_JAVA_MEM}|"             ./docker/solr/config/solr.in.sh
-    sed -i "s|<solr_port>|${SOLR_PORT}|"                     ./docker/solr/config/solr.in.sh
+    sed -i "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|"                     ./docker/solr/config/solr.in.sh
 
-    sed -i "s|<solr_port>|${SOLR_PORT}|g"                                   .env
-    sed -i "s|<solr_external_port>|${SOLR_EXTERNAL_PORT}|g"                 .env
-    sed -i "s|<logo_path>|${LOGO_PATH}|g"                                   .env
+    sed -i "s|<solr_gdz_port>|${SOLR_GDZ_PORT}|g"                                   .env
+    sed -i "s|<solr_gdz_external_port>|${SOLR_GDZ_EXTERNAL_PORT}|g"                 .env
+    sed -i "s|<gdz_logo_path>|${GDZ_LOGO_PATH}|g"                                   .env
+    sed -i "s|<nlh_logo_path>|${NLH_LOGO_PATH}|g"                                   .env
+    sed -i "s|<nlh_footer_path>|${NLH_FOOTER_PATH}|g"                                   .env
     sed -i "s|<font_path>|${FONT_PATH}|g"                                   .env
-    sed -i "s|<s3_provider>|${S3_PROVIDER}|g"                               .env
-    sed -i "s|<s3_aws_access_key_id>|${S3_AWS_ACCESS_KEY_ID}|g"             .env
-    sed -i "s|<s3_aws_secret_access_key>|${S3_AWS_SECRET_ACCESS_KEY}|g"     .env
-    sed -i "s|<s3_endpoint>|${S3_ENDPOINT}|g"                               .env
+    sed -i "s|<s3_sub_provider>|${S3_SUB_PROVIDER}|g"                            .env
+    sed -i "s|<s3_sub_aws_access_key_id>|${S3_SUB_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i "s|<s3_sub_aws_secret_access_key>|${S3_SUB_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i "s|<s3_sub_region>|${S3_SUB_REGION}|g"                            .env
+    sed -i "s|<s3_sub_endpoint>|${S3_SUB_ENDPOINT}|g"                            .env
+    sed -i "s|<s3_nlh_provider>|${S3_NLH_PROVIDER}|g"                            .env
+    sed -i "s|<s3_nlh_aws_access_key_id>|${S3_NLH_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i "s|<s3_nlh_aws_secret_access_key>|${S3_NLH_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i "s|<s3_nlh_region>|${S3_NLH_REGION}|g"                            .env
+    sed -i "s|<s3_nlh_endpoint>|${S3_NLH_ENDPOINT}|g"                            .env
+    sed -i "s|<s3_digizeit_provider>|${S3_DIGIZEIT_PROVIDER}|g"                            .env
+    sed -i "s|<s3_digizeit_aws_access_key_id>|${S3_DIGIZEIT_AWS_ACCESS_KEY_ID}|g"          .env
+    sed -i "s|<s3_digizeit_aws_secret_access_key>|${S3_DIGIZEIT_AWS_SECRET_ACCESS_KEY}|g"  .env
+    sed -i "s|<s3_digizeit_region>|${S3_DIGIZEIT_REGION}|g"                            .env
+    sed -i "s|<s3_digizeit_endpoint>|${S3_DIGIZEIT_ENDPOINT}|g"                            .env
     sed -i "s|<solr_java_mem>|${SOLR_JAVA_MEM}|"                            .env
     sed -i "s|<local_host>|${LOCAL_HOST}|"                                  .env
     sed -i "s|<service_host>|${SERVICE_HOST}|"                              .env
     sed -i "s|<redis_host>|${REDIS_HOST}|"                                  .env
-    sed -i "s|<solr_host>|${SOLR_HOST}|"                                    .env
+    sed -i "s|<solr_gdz_host>|${SOLR_GDZ_HOST}|"                                    .env
+    sed -i "s|<solr_nlh_host>|${SOLR_NLH_HOST}|"                                    .env
     sed -i "s|<graylog_host>|${GRAYLOG_HOST}|"                              .env
     sed -i "s|<graylog_port>|${GRAYLOG_PORT}|"                              .env
-    sed -i "s|<solr_core>|${solr_core}|"                                    .env
-    sed -i "s|<solr_core2>|${solr_core2}|"                                  .env
+    sed -i "s|<solr_gdz_core>|${solr_gdz_core}|"                                    .env
+    sed -i "s|<solr_gdz_core2>|${solr_gdz_core2}|"                                  .env
+    sed -i "s|<solr_nlh_core>|${solr_nlh_core}|"                                    .env
     sed -i "s|<service_verticle>|${SERVICE_VERTICLE}|"                      .env
     sed -i "s|<indexer_verticle>|${INDEXER_VERTICLE}|"                      .env
-    sed -i "s|<converter_verticle>|${CONVERTER_VERTICLE}|"                  .env
+    sed -i "s|<converter_work_verticle>|${CONVERTER_WORK_VERTICLE}|"                  .env
+    sed -i "s|<converter_img_log_verticle>|${CONVERTER_IMG_LOG_VERTICLE}|"                  .env
+    sed -i "s|<converter_img_full_verticle>|${CONVERTER_IMG_FULL_VERTICLE}|"                  .env
     sed -i "s|<verticle_home>|${VERTICLE_HOME}|"                            .env
     sed -i "s|<converter_vertx_options>|${CONVERTER_VERTX_OPTIONS}|"        .env
 fi
@@ -160,7 +195,7 @@ fi
 #docker-compose build --force-rm  importer_indexer importer_converter
 #docker-compose stop
 #docker-compose rm -f
-#docker-compose up -d importer_log_converter importer_converter importer_indexer
+#docker-compose up -d --scale  importer_converter=1 importer_converter importer_log_converter importer_indexer
 
 # local
 #docker-compose build --force-rm
@@ -171,5 +206,4 @@ fi
 
 
 
-# docker-compose up -d --scale  importer_converter=2 importer_converter importer_indexer
 
